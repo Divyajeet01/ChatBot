5c49eb82004b73a8c40bc83d3f3763206285a4e8
Divyajeet01 <158145903+Divyajeet01@users.noreply.github.com>
2024-06-01 23:16:41 +0530

    friday.py

--- chatbot/javis.py
+++ chatbot/javis.py
@@ -5,137 +5,110 @@ import wikipedia
 import webbrowser
 import smtplib
 from googlesearch import search
+import random
 
-engine=pyttsx3.init('sapi5')
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 150)
 
-voices=engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
-rate=engine.getProperty('rate')
-engine.setProperty('rate',200)
+reply = ['hello sir', 'welcome back sir', 'hi, how may I help you', 'good to see you again sir']
+reply_1 = ['bye sir', 'Sayonara', 'see you again sir', 'have a good day sir']
 
 def speak(audio):
+    """Function to make the assistant speak the given text."""
     engine.say(audio)
     engine.runAndWait()
 
 def wish():
-    time=int(datetime.datetime.now().hour)
-    if time>=0 and time<12:
-        speak("good morning")
-    elif time>=12 and time<18:
-        speak("good afternoon")
-    else :
-        speak("good evening")
-    speak("i am Friday. how may i help u")
+    """Function to greet the user based on the time of day."""
+    time = int(datetime.datetime.now().hour)
+    if 0 <= time < 12:
+        speak("Good morning")
+    elif 12 <= time < 18:
+        speak("Good afternoon")
+    else:
+        speak("Good evening")
+    speak("I am Friday. How may I help you?")
+
 def takecommand():
-    a=sr.Recognizer()
+    """Function to take voice commands from the user."""
+    recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        print ("Listening....")
-        a.pause_threshold=0.5
-        audio=a.listen(source)
-
+        print("Listening...")
+        recognizer.pause_threshold = 0.5
+        audio = recognizer.listen(source)
     try:
-        print("Recongnizing >>>")
-        q=a.recognize_google(audio,language="en")
-        print(f"user said : {q}\n")
+        print("Recognizing...")
+        query = recognizer.recognize_google(audio, language="en")
+        print(f"User said: {query}\n")
     except Exception as e:
-        print("Say again....")
+        print("Say again...")
         return "none"
-    return q
+    return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('divyajeetbiswal@gmail.com', 's@odisha12')
-    server.sendmail('divyajeetbiswal@gmail.com', to, content)
-    server.close()
+def send_Email(to, content):
 
-
-
-if __name__=="__main__":
+    sender = "divyajeetbiswal@gmail.com"
     try:
-        wish()
-        while True:
-            t=takecommand().lower()
-        #only responde if name taken
-            if "friday" in t:
-                t=t.replace("friday"," ")
-                if "wikipedia" in t:
-                    speak("Searching Wikipedia")
-                    t=t.replace ("wikipedia","")
-                    result=wikipedia.summary(t,sentences=2)
-                    print(result)
-                    speak(result)
-                elif "time" in t:
-                    x=datetime.datetime.now()
-                    time= x.strftime("%H:%M")
-                    time=time.replace(":"," ")
-                    if int(datetime.datetime.now().hour)<12:
-                        time=time+" a m"
-                    else:
-                        time=time+" p m"
-            
-                    speak(time)
-                elif "hello" in t:
-                    speak("hello,nice to meet u")
-                elif "play"  in t:
-                    t=t.replace("play","")
-                    t=t+" song"
-                    print(t)
-                    for i in search(t,tld = 'com',lang = 'en',num = 1,start = 0,stop = 1,pause = 2.0,):  
-                        print(i)
-                    webbrowser.open(i)
-
-                elif "open youtube" in t:
-                    webbrowser.open("youtube.com")  
-                elif "search " in t:
-                    t=t.replace("search","")
-                    print(t)
-                    for i in search(t,tld = 'com',lang = 'en',num = 1,start = 0,stop = 1,pause = 2.0,):  
-                        print(i)
-                    webbrowser.open(i) 
-                
-                elif "email" in t:
-                    from email.message import EmailMessage
-                    msg = EmailMessage()
-                    msg.set_content("hello")
-                    msg['Subject'] = 'The contents of '
-                    msg['From']='divyajeetbiswal@gmail.com'
-                    msg['To']="divyajeetbiswal@gmail.com"
-                    try:
-                        s = smtplib.SMTP('localhost')
-                        s.send_message(msg)
-                        s.quit()
-                    except Exception as f:
-                        print(f)
-                    '''elif "email" in t:
-                    try:
-                        speak("reciever email address ")
-                        to=takecommand()
-                        to=to.replace(" ","")
-                        speak("content ")
-                        a=takecommand()
-                        sendEmail(to,a)
-                    except Exception as x:
-                        print(x)
-                        speak("email not send")'''
-                    
-
-
-                elif "exit"  in t:
-
-                    speak("sayonara")
-                    exit()
-                    
-                   
-                else :
-                    for i in search(t,tld = 'com',lang = 'en',num = 1,start = 0,stop = 1,pause = 2.0,):  
-                        print(i)
-                    webbrowser.open(i)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender, 'jvgi zlzf lkiv ggdh')  
+        server.sendmail(sender, to, content)
+        speak("Email has been sent")
     except Exception as e:
-        print (e)
-                    
+        print(e)
+        speak("Sorry, I am not able to send this email")
+    finally:
+        server.quit()
 
-
-
-    
+if __name__ == "__main__":
+    wish()
+    while True:
+        try:
+            query = takecommand().lower()
+            if "friday" in query:
+                query = query.replace("friday", "")
+                if "wikipedia" in query:
+                    speak("Searching Wikipedia")
+                    query = query.replace("wikipedia", "")
+                    results = wikipedia.summary(query, sentences=1)
+                    print(results)
+                    speak(results)
+                elif "time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")
+                    speak(f"Sir, the time is {strTime}")
+                elif "hello" in query:
+                    speak(random.choice(reply))
+                elif "play" in query:
+                    query = query.replace("play", "")
+                    query += " song"
+                    for result in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=2.0):
+                        print(result)
+                        webbrowser.open(result)
+                elif "open youtube" in query:
+                    webbrowser.open("youtube.com")
+                elif "search" in query:
+                    query = query.replace("search", "")
+                    for result in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=2.0):
+                        print(result)
+                        webbrowser.open(result)
+                elif 'email' in query:
+                    try:
+                        to = input('Enter Receiver Email: ')
+                        sub = input('Enter Subject: ')
+                        msg = input('Enter Message: ')
+                        content = f"Subject: {sub}\n\n{msg}"
+                        send_Email(to, content)
+                    except Exception as e:
+                        print(e)
+                        speak("Sorry, I am not able to send this email")
+                elif "exit" in query or "bye" in query or "shutdown" in query:
+                    speak(random.choice(reply_1))
+                    exit()
+                else:
+                    for result in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=2.0):
+                        print(result)
+                        webbrowser.open(result)
+        except Exception as e:
+            print(e)
